eb0339fae2381407694ea9afe550406e4b7c20b1
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-05-08 18:48:12 +0300

    Nativewind Setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView
+      className="flex-1 items-center text-red-500 justify-center bg-orange-500"
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
---
+++ constants/icons.ts
@@ -0,0 +1,55 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import person from "@/assets/icons/person.png";
+import logo from "@/assets/icons/logo.png";
+import save from "@/assets/icons/save.png";
+import star from "@/assets/icons/star.png";
+import play from "@/assets/icons/play.png";
+import arrow from "@/assets/icons/arrow.png";
+import activity from "@/assets/icons/activity.png";
+import add from "@/assets/icons/add.png";
+import adobe from "@/assets/icons/adobe.png";
+import back from "@/assets/icons/back.png";
+import canva from "@/assets/icons/canva.png";
+import home1 from "@/assets/icons/home1.png";
+import claude from "@/assets/icons/claude.png";
+import dropbox from "@/assets/icons/dropbox.png";
+import figma from "@/assets/icons/figma.png";
+import github from "@/assets/icons/github.png";
+import medium from "@/assets/icons/medium.png";
+import menu from "@/assets/icons/menu.png";
+import notion from "@/assets/icons/notion.png";
+import openai from "@/assets/icons/openai.png";
+import plus from "@/assets/icons/plus.png";
+import setting from "@/assets/icons/setting.png";
+import spotify from "@/assets/icons/spotify.png";
+import wallet from "@/assets/icons/wallet.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+  home1,
+    wallet,
+    setting,
+    activity,
+    add,
+    back,
+    menu,
+    plus,
+    notion,
+    dropbox,
+    openai,
+    adobe,
+    medium,
+    figma,
+    spotify,
+    github,
+    claude,
+    canva,
+};
